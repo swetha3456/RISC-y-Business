db40c06e77500166a2a3381a419683dd023519ac
swetha3456 <swetha.murali@iiitb.ac.in>
2024-06-15 03:29:39 +0530

    reverted to old code

--- prevention.c
+++ prevention.c
@@ -14,8 +14,6 @@
 #define SERVO_PIN GPIO_Pin_2
 #define LDR_PIN GPIO_Pin_1
 #define CLOCK_ENABLE RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE)
-#define MAX_DURATION "000000000000000000000000"
-#define DELAY 30
 
 void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
 void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
@@ -64,9 +62,6 @@ void open_door() {
 	GPIO_WriteBit(GPIO_PORT, GREEN_LED, 1);
 	GPIO_WriteBit(GPIO_PORT, RED_LED, 0);
 	Delay_Ms(7000);
-}
-
-void lock_door() {
 	GPIO_WriteBit(GPIO_PORT, GREEN_LED, 0);
 	GPIO_WriteBit(GPIO_PORT, RED_LED, 1);
 	TIM1_PWMOut_Init( 100, 2000-1, 75 );
@@ -100,146 +95,56 @@ int main(void) {
 	GPIO_InitStructure.GPIO_Mode =  GPIO_Mode_IPU;
 	GPIO_Init(GPIO_PORT, &GPIO_InitStructure);
 
-	int locked = 0;
-	char duration[50] = "";
-	char expected_duration[50] = "";
-	open_door();
+	uint8_t locked = 1;
+	char duration[5] = "";
 
-	while (1) {
-		GPIO_WriteBit(GPIO_PORT, GREEN_LED, ~locked);
-    	GPIO_WriteBit(GPIO_PORT, RED_LED, locked);
-		int val = GPIO_ReadInputDataBit(GPIOD, LDR_PIN); 
-		
-		while (val == 0) {  
-			GPIO_WriteBit(GPIO_PORT, GREEN_LED, ~locked);
-			GPIO_WriteBit(GPIO_PORT, RED_LED, locked);
+	GPIO_WriteBit(GPIO_PORT, GREEN_LED, ~locked);
+    GPIO_WriteBit(GPIO_PORT, RED_LED, locked);
+	int val = GPIO_ReadInputDataBit(GPIOD, LDR_PIN); 
 
-			// Reading from the LDR sensor
-			int val2 = GPIO_ReadInputDataBit(GPIOD, LDR_PIN);
-			char val2Str[5];
-			sprintf(val2Str, "%d", val2);
-			strcat(duration, val2Str);
-		
-			// Set/release lock
-			if (strcmp(duration, "0001") == 0) {
-				duration[0] = '\0';
+	while (val == 0) {  
+		// Reading from the LDR sensor
+	    int val2 = GPIO_ReadInputDataBit(GPIOD, LDR_PIN);
+	    char val2Str[5];
+        sprintf(val2Str, "%d", val2);
+        strcat(duration, val2Str);
+		printf("%d", val2);
+	 
+		// Correct duration
+	    if (strcmp(duration, "0001") == 0) {
+	    	open_door();
+			duration[0] = '\0';
+			break;
+	    }
 
-				// Release lock
-				if (locked) {
-					while (GPIO_ReadInputDataBit(GPIOD, LDR_PIN)) {}
-
-					// Reading duration again
-					while (1) {
-						int val3 = GPIO_ReadInputDataBit(GPIOD, LDR_PIN);
-						char val3Str[5];
-						sprintf(val3Str, "%d", val3);
-						strcat(duration, val3Str);
-
-						// Correct duration
-						if (strcmp(duration, expected_duration) == 0) {
-							duration[0] = '\0';
-							expected_duration[0] = '\0';
-							locked = 0;
-							open_door();
-							break;
-						}
-
-						// Too long
-						else if (strcmp(duration, MAX_DURATION) == 0) {
-							GPIO_WriteBit(GPIO_PORT, BUZZER, 1);
-							Delay_Ms(200);
-							GPIO_WriteBit(GPIO_PORT, BUZZER, 0);
-							duration[0] = '\0';
-							break;
-						}
-
-						// Too short
-						if (val3 == 1) {
-							GPIO_WriteBit(GPIO_PORT, BUZZER, 1);
-							Delay_Ms(200);
-							GPIO_WriteBit(GPIO_PORT, BUZZER, 0);
-							duration[0] = '\0';
-							break; 
-						}
-
-						Delay_Ms(DELAY);
-					}
-
+		// Too long
+		else if (strcmp(duration, "0000") == 0) {
+	    	GPIO_WriteBit(GPIO_PORT, BUZZER, 1);
+			Delay_Ms(200);
+			GPIO_WriteBit(GPIO_PORT, BUZZER, 0);
+			duration[0] = '\0';
+			break;
+	    }
+	  
+	  	// Too short
+	    if (val2 == 1) {
+			for (int x = 0; x < 8; x++) {
+				if (GPIO_ReadInputDataBit(GPIOD, LDR_PIN) == 0) {
+					Delay_Ms(60000); // Pause for 1 minute before allowing to retry
 					break;
 				}
 
-				// Set lock duration
-				else {
-					expected_duration[0] = '\0';
-					locked = 1;
-					while (GPIO_ReadInputDataBit(GPIOD, LDR_PIN)) {}
-
-					// Reading duration again
-					while (1) {
-						int val3 = GPIO_ReadInputDataBit(GPIOD, LDR_PIN);
-						char val3Str[5];
-						sprintf(val3Str, "%d", val3);
-						strcat(expected_duration, val3Str);
-
-						// Trying to set a duration that is too long
-						if (strcmp(expected_duration, MAX_DURATION) == 0) {
-							GPIO_WriteBit(GPIO_PORT, BUZZER, 1);
-							Delay_Ms(200);
-							GPIO_WriteBit(GPIO_PORT, BUZZER, 0);
-							expected_duration[0] = '\0';
-							break;
-						}
-
-						if (val3 == 1) {
-							lock_door();
-							break;
-						}
-
-						Delay_Ms(DELAY);
-					}
-
-					break;
-				}
+				Delay_Ms(10);
 			}
 
-			// Unlock door with correct duration
-			else if (locked && strcmp(duration, expected_duration) == 0) {
-				duration[0] = '\0';
-				locked = 1;
-				open_door(); 
-				lock_door(); // Close door after 7 second delay
-				break;
-			}
-
-			// Too long
-			else if (strcmp(duration, MAX_DURATION) == 0) {
-				GPIO_WriteBit(GPIO_PORT, BUZZER, 1);
-				Delay_Ms(200);
-				GPIO_WriteBit(GPIO_PORT, BUZZER, 0);
-				duration[0] = '\0';
-				break;
-			}
-		
-			// Too short
-			else if (val2 == 1) {
-				for (int x = 0; x < 8; x++) {
-                    if (GPIO_ReadInputDataBit(GPIOD, LDR_PIN) == 0) {
-                        Delay_Ms(60000); // Pause for 1 minute before allowing to retry
-                        break;
-                    }
-
-                    Delay_Ms(10);
-                }
-
-				GPIO_WriteBit(GPIO_PORT, BUZZER, 1);
-				Delay_Ms(200);
-				GPIO_WriteBit(GPIO_PORT, BUZZER, 0);
-				duration[0] = '\0';
-				break;
-			}
-		
-			Delay_Ms(DELAY);
-		}
+			GPIO_WriteBit(GPIO_PORT, BUZZER, 1);
+			Delay_Ms(200);
+			GPIO_WriteBit(GPIO_PORT, BUZZER, 0);
+	    	duration[0] = '\0';
+			break;
+	    }
+	
+	    Delay_Ms(15);
 	}
 }
 
